f4d1fb21bef987fb10309b051202a0f35f15e917
Abbie Ma <ama@myameriflex.com>
2020-01-23 16:21:38 -0600

    Add project files.

---
+++ iTrellis.CarDealership.ApiTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace iTrellis.CarDealership.ApiTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        { 
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
